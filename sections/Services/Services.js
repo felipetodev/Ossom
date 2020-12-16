7f6999e165c21deab05d3f8b26175068a01ea568
felipetodev <fe.ossandon.u@gmail.com>
2020-12-15 23:30:02 -0300

    slider

--- sections/Services/Services.js
+++ sections/Services/Services.js
@@ -1,9 +1,70 @@
-import styles from './Services.module.css'
 import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { wrap } from '@popmotion/popcorn'
+import styles from './Services.module.css'
+import { data, buttons } from './ServicesData'
 
 export default function Services() {
+    const [ [panel, direction], setPanel ] = useState([0, 0])
+    const index = wrap(0, data.length, panel)
+    
+    const paginate = (newDirection) => {
+        setPanel([panel + newDirection, newDirection])
+    }
 
-    const [ panel, setPanel ] = useState(0)
+    const variants = {
+        enter: (direction) => {
+          return {
+            x: direction > 0 ? 1000 : -1000,
+            opacity: 0
+          }
+        },
+        center: {
+          zIndex: 1,
+          x: 0,
+          opacity: 1
+        },
+        exit: (direction) => {
+          return {
+            zIndex: 0,
+            x: direction < 0 ? 1000 : -1000,
+            opacity: 0
+          }
+        }
+    }
+
+    const swipeConfidenceThreshold = 10000
+    const swipePower = (offset, velocity) => {
+        return Math.abs(offset) * velocity;
+    }
+
+    const displayPanel = (key) => {
+        console.log(key)
+        return (
+            <div className={styles.servicesContainer}>
+                <div className={styles.service}>
+                    <div className={styles.summaryContainer}>
+                        <div className={styles.summary}>
+                            <h4>{data[key].title}</h4>
+                            <p>{data[key].summary}</p>
+                        </div>
+                    </div>
+                    <div className={styles.details}>
+                        {data[key].details.map(item => (
+                            <>
+                                <h4>{item.title}</h4>
+                                <ul>
+                                    {item.points.map(point => (
+                                        <li>{point}</li>
+                                    ))}
+                                </ul>
+                            </>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <section className={styles.container}>
@@ -14,17 +75,48 @@ export default function Services() {
                 </p>
             </div>
             <div className={styles.buttonGroup}>
-                <button className={styles.active}>Planificación del Producto</button>
-                <button>Planificación del Desarrollo</button>
-                <button>Testing del Producto</button>
-                <button>Escalamiento del Producto</button>
+                {buttons.map((button, i) => (
+                    <button
+                    key={button}
+                    className={i === index ? styles.active : ''}
+                    onClick={() => setPanel([i, -1])}
+                    >{button}</button>
+                ))}
             </div>
-            <div className={styles.services}>
-                <div className={styles.servicesContainer}>
-                    <div className={styles.service}>
-                        <h1>Descripción Producto</h1>
-                    </div>
-                </div>
+            <div style={{ position: "relative" }}>
+                <AnimatePresence initial={false} custom={direction}>
+                    <motion.div
+                        dragConstraints={{ left: 0, right: 0 }}
+                        style={{ 
+                            width: "100%",
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                        }}
+                        key={panel}
+                        drag="x"
+                        dragElastic={1}
+                        variants={variants}
+                        initial="enter"
+                        animate="center"
+                        exit="exit"
+                        transition={{
+                            x: { type: "spring", stiffness: 300, damping: 30 },
+                            opacity: { duration: 0.2 }
+                        }}
+                        onDragEnd={(e, { offset, velocity }) => {
+                            const swipe = swipePower(offset.x, velocity.x);
+                
+                            if (swipe < -swipeConfidenceThreshold) {
+                              paginate(1);
+                            } else if (swipe > swipeConfidenceThreshold) {
+                              paginate(-1);
+                            }
+                        }}
+                    >
+                        {displayPanel(index)}
+                    </motion.div>
+                </AnimatePresence>
             </div>
         </section>
     )
---
+++ sections/Services/ServicesData.js
@@ -0,0 +1,105 @@
+export const data = [
+    {
+        title: "Planificación del Producto",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut nisl at ante aliquam venenatis. Duis consectetur faucibus maximus. Praesent nec consectetur felis, hendrerit sagittis arcu. Mauris dapibus, magna et dictum scelerisque, lorem odio posuere libero, consectetur accumsan sapien odio nec justo.",
+        details: [
+            {
+                title: "Definición",
+                    points: [
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ]
+
+            },
+            {
+                title: "Diseño",
+                    points: [
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ]
+
+            },
+        ]
+    },
+    {
+        title: "Planificación del Desarrollo",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut nisl at ante aliquam venenatis. Duis consectetur faucibus maximus. Praesent nec consectetur felis, hendrerit sagittis arcu. Mauris dapibus, magna et dictum scelerisque, lorem odio posuere libero, consectetur accumsan sapien odio nec justo.",
+        details: [
+            {
+                title: "Definición",
+                    points: [
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ]
+
+            },
+            {
+                title: "Diseño",
+                    points: [
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ]
+
+            },
+        ]
+    },
+    {
+        title: "Testing del Producto",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut nisl at ante aliquam venenatis. Duis consectetur faucibus maximus. Praesent nec consectetur felis, hendrerit sagittis arcu. Mauris dapibus, magna et dictum scelerisque, lorem odio posuere libero, consectetur accumsan sapien odio nec justo.",
+        details: [
+            {
+                title: "Definición",
+                    points: [
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ]
+
+            },
+            {
+                title: "Diseño",
+                    points: [
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ]
+
+            },
+        ]
+    },
+    {
+        title: "Escalamiento del Producto",
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut nisl at ante aliquam venenatis. Duis consectetur faucibus maximus. Praesent nec consectetur felis, hendrerit sagittis arcu. Mauris dapibus, magna et dictum scelerisque, lorem odio posuere libero, consectetur accumsan sapien odio nec justo.",
+        details: [
+            {
+                title: "Definición",
+                    points: [
+                        "Escalamiento ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ],
+
+            },
+            {
+                title: "Diseño",
+                    points: [
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                        "Lorem ipsum dolor sit amet adipiscing elit.",
+                    ],
+
+            },
+        ],
+    },
+]
+
+export const buttons = [
+    'Planificación del Producto',
+    'Planificación del Desarrollo',
+    'Testing del Producto',
+    'Escalamiento del Producto',
+]
